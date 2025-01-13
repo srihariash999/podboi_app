3d14951e80fade558e0682138f42f8d954fd64f5
Srihari Ayapilla <srihariash@gmail.com>
2025-01-13 20:10:49 +0530

    listening history saving updates

--- lib/Services/database/listening_history_box_controller.dart
+++ lib/Services/database/listening_history_box_controller.dart
@@ -6,6 +6,19 @@ class ListeningHistoryBoxController {
   // Method to call from anywhere in app to store an episode to listening history.
   static Future<bool> saveEpisodeToHistory(ListeningHistoryData data) async {
     Box _box = Hive.box<ListeningHistoryData>(K.boxes.listeningHistoryBox);
+
+    List<ListeningHistoryData> items = [];
+
+    for (var i in _box.values.toList()) {
+      items.add(i as ListeningHistoryData);
+    }
+
+    if (items.length >= 1000) {
+      // remove the oldest item
+      items.sort((a, b) => a.listenedOn.compareTo(b.listenedOn));
+      _box.delete(items[0].episodeData.guid.toString());
+    }
+
     await _box.put(data.episodeData.guid.toString(), data);
     print(" saved episode to history with id: ${data.episodeData.guid}");
     return true;
@@ -20,6 +33,8 @@ class ListeningHistoryBoxController {
       for (var i in list) {
         historyList.add(i as ListeningHistoryData);
       }
+      // sort the list by date
+      historyList.sort((a, b) => b.listenedOn.compareTo(a.listenedOn));
       return historyList;
     } catch (e) {
       print("error in getting history list: $e");
