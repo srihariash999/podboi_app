e63156877deaa62a3cd69b6c95697f772a723072
Srihari Ayapilla <srihariash@gmail.com>
2025-01-18 23:18:08 +0530

    Edit name text field bug in profile page fix.

--- lib/UI/profile_page.dart
+++ lib/UI/profile_page.dart
@@ -109,6 +109,7 @@ class ProfileScreen extends StatelessWidget {
                                 onTap: () async {
                                   showModalBottomSheet<void>(
                                     context: context,
+                                    isScrollControlled: true,
                                     builder: (BuildContext context) {
                                       return ProfileEditWidget(
                                         userName: _name,
@@ -403,206 +404,203 @@ class _ProfileEditWidgetState extends State<ProfileEditWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: MediaQuery.of(context).size.height * 0.5,
       color: Theme.of(context).colorScheme.primary.withOpacity(0.8),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width * 0.85,
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.only(left: 6.0),
-              child: Text(
-                "Change Name",
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w700,
-                  color:
-                      Theme.of(context).colorScheme.secondary.withOpacity(0.9),
-                  fontFamily: 'Segoe',
-                ),
+      margin: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          SizedBox(height: MediaQuery.of(context).size.height * 0.07),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.85,
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(left: 6.0),
+            child: Text(
+              "Change Name",
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w700,
+                color: Theme.of(context).colorScheme.secondary.withOpacity(0.9),
+                fontFamily: 'Segoe',
               ),
             ),
-            SizedBox(
-              height: 12.0,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.85,
-              child: Theme(
-                data: ThemeData(
-                  primaryColor:
-                      Theme.of(context).primaryColor.withOpacity(0.30),
+          ),
+          SizedBox(
+            height: 12.0,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.85,
+            child: Theme(
+              data: ThemeData(
+                primaryColor: Theme.of(context).primaryColor.withOpacity(0.30),
+              ),
+              child: TextField(
+                controller: _nameController,
+                cursorColor: Theme.of(context).colorScheme.secondary,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w700,
+                  color: Theme.of(context).colorScheme.secondary,
                 ),
-                child: TextField(
-                  controller: _nameController,
-                  cursorColor: Theme.of(context).colorScheme.secondary,
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w700,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    LineIcons.user,
                     color: Theme.of(context).colorScheme.secondary,
                   ),
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(
-                      LineIcons.user,
-                      color: Theme.of(context).colorScheme.secondary,
-                    ),
-                    isCollapsed: true,
-                    contentPadding: const EdgeInsets.symmetric(vertical: 16.0),
-                    hintText: " What do we call you ? ",
-                    alignLabelWithHint: true,
-                    hintStyle: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w500,
-                      color: Theme.of(context)
+                  isCollapsed: true,
+                  contentPadding: const EdgeInsets.symmetric(vertical: 16.0),
+                  hintText: " What do we call you ? ",
+                  alignLabelWithHint: true,
+                  hintStyle: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w500,
+                    color: Theme.of(context)
+                        .colorScheme
+                        .secondary
+                        .withOpacity(0.50),
+                  ),
+                  fillColor: Theme.of(context).highlightColor.withOpacity(0.4),
+                  filled: true,
+                  focusColor: Colors.black.withOpacity(0.30),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.85,
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(left: 6.0),
+            child: Text(
+              "Change avatar",
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w700,
+                color: Theme.of(context).colorScheme.secondary.withOpacity(0.9),
+                fontFamily: 'Segoe',
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _selectedAvatar = 'user';
+                  });
+                },
+                child: Icon(
+                  LineIcons.user,
+                  size: 52.0,
+                  color: _selectedAvatar == 'user'
+                      ? Theme.of(context).colorScheme.secondary
+                      : Theme.of(context)
                           .colorScheme
                           .secondary
-                          .withOpacity(0.50),
-                    ),
-                    fillColor:
-                        Theme.of(context).highlightColor.withOpacity(0.4),
-                    filled: true,
-                    focusColor: Colors.black.withOpacity(0.30),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
+                          .withOpacity(0.4),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _selectedAvatar = 'userNinja';
+                  });
+                },
+                child: Icon(
+                  LineIcons.userNinja,
+                  size: 52.0,
+                  color: _selectedAvatar == 'userNinja'
+                      ? Theme.of(context).colorScheme.secondary
+                      : Theme.of(context)
+                          .colorScheme
+                          .secondary
+                          .withOpacity(0.4),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _selectedAvatar = 'userAstronaut';
+                  });
+                },
+                child: Icon(
+                  LineIcons.userAstronaut,
+                  size: 52.0,
+                  color: _selectedAvatar == 'userAstronaut'
+                      ? Theme.of(context).colorScheme.secondary
+                      : Theme.of(context)
+                          .colorScheme
+                          .secondary
+                          .withOpacity(0.4),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 50.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomChipButtonWidget(
+                onTap: () => Navigator.pop(context),
+                child: Text(
+                  'Cancel',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w500,
+                    color: Theme.of(context).colorScheme.secondary,
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.85,
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.only(left: 6.0),
-              child: Text(
-                "Change avatar",
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w700,
-                  color:
-                      Theme.of(context).colorScheme.secondary.withOpacity(0.9),
-                  fontFamily: 'Segoe',
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _selectedAvatar = 'user';
-                    });
+              const SizedBox(width: 16.0),
+              Consumer(builder: (context, ref, child) {
+                bool _loading = ref.watch(
+                  profileController.select((value) => value.loading),
+                );
+                return CustomChipButtonWidget(
+                  onTap: () async {
+                    if (!_loading) {
+                      await ref.read(profileController.notifier).editProfile(
+                            name: _nameController.text.trim(),
+                            avatar: _selectedAvatar,
+                          );
+                      Navigator.pop(context);
+                    }
                   },
-                  child: Icon(
-                    LineIcons.user,
-                    size: 52.0,
-                    color: _selectedAvatar == 'user'
-                        ? Theme.of(context).colorScheme.secondary
-                        : Theme.of(context)
-                            .colorScheme
-                            .secondary
-                            .withOpacity(0.4),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _selectedAvatar = 'userNinja';
-                    });
-                  },
-                  child: Icon(
-                    LineIcons.userNinja,
-                    size: 52.0,
-                    color: _selectedAvatar == 'userNinja'
-                        ? Theme.of(context).colorScheme.secondary
-                        : Theme.of(context)
-                            .colorScheme
-                            .secondary
-                            .withOpacity(0.4),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _selectedAvatar = 'userAstronaut';
-                    });
-                  },
-                  child: Icon(
-                    LineIcons.userAstronaut,
-                    size: 52.0,
-                    color: _selectedAvatar == 'userAstronaut'
-                        ? Theme.of(context).colorScheme.secondary
-                        : Theme.of(context)
-                            .colorScheme
-                            .secondary
-                            .withOpacity(0.4),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 50.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CustomChipButtonWidget(
-                  onTap: () => Navigator.pop(context),
+                  color: Color(0xFF98c1d9).withOpacity(0.7),
                   child: Text(
-                    'Cancel',
+                    _loading ? 'Saving...' : 'Save',
                     style: TextStyle(
                       fontSize: 16.0,
                       fontWeight: FontWeight.w500,
                       color: Theme.of(context).colorScheme.secondary,
                     ),
                   ),
-                ),
-                const SizedBox(width: 16.0),
-                Consumer(builder: (context, ref, child) {
-                  bool _loading = ref.watch(
-                    profileController.select((value) => value.loading),
-                  );
-                  return CustomChipButtonWidget(
-                    onTap: () async {
-                      if (!_loading) {
-                        await ref.read(profileController.notifier).editProfile(
-                              name: _nameController.text.trim(),
-                              avatar: _selectedAvatar,
-                            );
-                        Navigator.pop(context);
-                      }
-                    },
-                    color: Color(0xFF98c1d9).withOpacity(0.7),
-                    child: Text(
-                      _loading ? 'Saving...' : 'Save',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w500,
-                        color: Theme.of(context).colorScheme.secondary,
-                      ),
-                    ),
-                  );
-                }),
-              ],
-            ),
-          ],
-        ),
+                );
+              }),
+            ],
+          ),
+        ],
       ),
     );
   }
