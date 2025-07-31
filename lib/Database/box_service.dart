2888263ea4d84efe1da039ba2372a1958a981114
Srihari Ayapilla <srihariash@gmail.com>
2025-07-31 19:55:10 +0530

    updated box service.

--- lib/Database/box_service.dart
+++ lib/Database/box_service.dart
@@ -8,9 +8,9 @@ class BoxService implements IBoxService {
   @override
   Future<Box<T>> getBox<T>(String boxName) async {
     try {
-      return Hive.openBox<T>(boxName);
-    } on HiveError catch (_) {
       return Hive.box<T>(boxName);
+    } on HiveError catch (_) {
+      return Hive.openBox<T>(boxName);
     } catch (e) {
       rethrow;
     }
